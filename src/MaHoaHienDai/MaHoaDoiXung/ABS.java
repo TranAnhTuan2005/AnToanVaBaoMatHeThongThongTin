68e29dc0b3c4852a8184ea0e9a8cd61428bc6fcb
TuanTran <tuan072005@gmail.com>
2026-05-01 23:42:23 +0700

    ma hoa hien dai doi xung

--- src/MaHoaHienDai/MaHoaDoiXung/ABS.java
+++ src/MaHoaHienDai/MaHoaDoiXung/ABS.java
@@ -1,6 +1,7 @@
 package MaHoaHienDai.MaHoaDoiXung;
 
 import javax.crypto.*;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -24,7 +25,7 @@ public class ABS {
 
 
     public byte[] encrypt(String text) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher=Cipher.getInstance("ABS");
+        Cipher cipher=Cipher.getInstance("");//mode and padding
         cipher.init(Cipher.ENCRYPT_MODE, this.key);
         byte[] data= text.getBytes(StandardCharsets.UTF_8);
         return cipher.doFinal(data);
@@ -37,11 +38,58 @@ public class ABS {
     }
 
     public String Decrypt(byte[] data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        Cipher cipher=Cipher.getInstance("ABS");
+        Cipher cipher=Cipher.getInstance("");//mode and padding
         cipher.init(Cipher.DECRYPT_MODE, this.key);
         byte[] bytes= cipher.doFinal(data);
         return new String(bytes, StandardCharsets.UTF_8);
     }
+
+    public boolean encryptFile(String src, String dec) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("");//mode and padding
+        cipher.init(Cipher.ENCRYPT_MODE, this.key);
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(src));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(dec));
+        CipherInputStream in = new CipherInputStream(input, cipher);
+        int i;
+        byte[] read = new byte[1024];
+        byte[] re = null;
+        while ((i = in.read(read)) != -1) {
+            output.write(read, 0, i);
+
+        }
+        read = cipher.doFinal();
+        if (read != null) {
+            output.write(read);
+        }
+        in.close();
+        output.flush();
+        output.close();
+        return true;
+    }
+
+    public boolean decryptFile(String src, String dec) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("");//mode and padding
+        cipher.init(Cipher.DECRYPT_MODE, this.key);
+        BufferedInputStream input = new BufferedInputStream(new FileInputStream(src));
+        BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(dec));
+        CipherInputStream in = new CipherInputStream(input, cipher);
+        int i;
+        byte[] read = new byte[1024];
+        byte[] re = null;
+        while ((i=input.read())!=-1){
+            output.write(read,0,i);
+        }
+        read= cipher.doFinal();
+        if (read != null) {
+            output.write(read);
+        }
+        in.close();
+        output.flush();
+        output.close();
+        return true;
+    }
+
+
 
 
 }
